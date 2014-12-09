7e534c885c097eb0e35d9b60da4cbf71f9e71710
Gabriel Florit <gabrielflorit@gmail.com>
2014-12-09 18:42:30 -0500

    point to right jsonp feed

--- graphics/map/js/main.js
+++ graphics/map/js/main.js
@@ -14,62 +14,13 @@ var map = L.map($map.get(0), {
 	scrollWheelZoom: false
 }).setView([42.25847871, -71.81544179], 8);
 
-// Get outages data.
-var data = require('../../../../outages_scraper/outages.json');
-var towns = data.towns;
-var dates = data.dates;
-
-// Convert towns topojson to geojson.
-var townsTopojson = require('../../../data/output/TOWNS.json');
-var townsGeojson = topojson.feature(townsTopojson, townsTopojson.objects.TOWNS);
-
-// Bind outages data to towns.
-towns.forEach(function(town) {
-
-	var match = _.find(townsGeojson.features, function(feature) {
-		return feature.properties.TOWN === town.town;
-	});
-
-	// Calculate total customers.
-	var total = _.chain(town.companies)
-		.pluck('Total Cust.')
-		.reduce(function(a, b) {
-			return a + b;
-		})
-		.value();
-
-	var out = _.chain(town.companies)
-		.pluck('Cust. Out')
-		.reduce(function(a, b) {
-			return a + b;
-		})
-		.value();
-
-	match.properties.out = out;
-	match.properties.total = total;
-	match.properties.companies = town.companies;
-});
-
-var OUT = _.chain(townsGeojson.features)
-	.map(function(feature) {
-		return feature.properties.out;
-	})
-	.filter(function(out) {
-		return out;
-	})
-	.reduce(function(a, b) {
-		return a + b;
-	})
-	.value();
-
-// Populate the 'totals' element.
-$('.subhed .totals span').html(numberWithCommas(OUT));
-
 // Construct the chroma color interpolator using bezier curves.
 var interpolator = chroma.interpolate.bezier(['#FFFFFF', '#6e1315']);
 
 // Also correct lightness. See https://github.com/gka/chroma.js for more information.
 var scale = chroma.scale(interpolator).correctLightness(true);
+
+var popup;
 
 // Return default style based on feature properties.
 function style(feature) {
@@ -89,8 +40,6 @@ function style(feature) {
 		fillColor: color
 	};
 }
-
-var popup;
 
 function getPopupContent(feature) {
 	var town = feature.properties.TOWN.toLowerCase();
@@ -161,27 +110,7 @@ function resetHighlight(e) {
 	geojson.resetStyle(e.target);
 }
 
-// Add towns color layer to map.
-var geojson = L.geoJson(townsGeojson, {
-	style: style,
-	onEachFeature: function(feature, layer) {
-		layer.on({
-			mouseover: highlightFeature,
-			click: highlightFeature
-		});
-	}
-}).addTo(map);
-
-// Unorthodox layer creation so we can have baselayer above interaction layer.
-// This comes from http://bl.ocks.org/rsudekum/5431771.
-var topPane = map._createPane('leaflet-top-pane', map.getPanes().mapPane);
-var topLayer = L.tileLayer('http://{s}.tiles.mapbox.com/v3/gabriel-florit.207de5da/{z}/{x}/{y}.png', {
-	minZoom: 7,
-	maxZoom: 13
-}).addTo(map);
-topPane.appendChild(topLayer.getContainer());
-topLayer.setZIndex(6);
-
+var geojson;
 
 // Create a Leaflet control for the legend.
 var MyControl = L.Control.extend({
@@ -206,16 +135,99 @@ if (!Modernizr.touch) {
 	map.addControl(new MyControl());
 }
 
-// Construct a new date with no minutes or seconds.
-var date = new Date(_.chain(dates)
-	.pluck('date')
-	.sortBy(function(date) {
-		return date;
-	})
-	.first()
-	.value());
+// Get outages data.
+window.outages = function(data) {
 
-var hourDate = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes());
+	var towns = data.towns;
+	var dates = data.dates;
 
-// Populate the 'updated' element.
-$('.updated-timestamp').html('Updated ' + [APDateTime.time(hourDate), APDateTime.date(hourDate)].join(', '));
+	// Convert towns topojson to geojson.
+	var townsTopojson = require('../../../data/output/TOWNS.json');
+	var townsGeojson = topojson.feature(townsTopojson, townsTopojson.objects.TOWNS);
+
+	// Bind outages data to towns.
+	towns.forEach(function(town) {
+
+		var match = _.find(townsGeojson.features, function(feature) {
+			return feature.properties.TOWN === town.town;
+		});
+
+		// Calculate total customers.
+		var total = _.chain(town.companies)
+			.pluck('Total Cust.')
+			.reduce(function(a, b) {
+				return a + b;
+			})
+			.value();
+
+		var out = _.chain(town.companies)
+			.pluck('Cust. Out')
+			.reduce(function(a, b) {
+				return a + b;
+			})
+			.value();
+
+		match.properties.out = out;
+		match.properties.total = total;
+		match.properties.companies = town.companies;
+	});
+
+	var OUT = _.chain(townsGeojson.features)
+		.map(function(feature) {
+			return feature.properties.out;
+		})
+		.filter(function(out) {
+			return out;
+		})
+		.reduce(function(a, b) {
+			return a + b;
+		})
+		.value();
+
+	// Populate the 'totals' element.
+	$('.subhed .totals span').html(numberWithCommas(OUT));
+
+	var popup;
+
+	// Add towns color layer to map.
+	geojson = L.geoJson(townsGeojson, {
+		style: style,
+		onEachFeature: function(feature, layer) {
+			layer.on({
+				mouseover: highlightFeature,
+				click: highlightFeature
+			});
+		}
+	}).addTo(map);
+
+	// Unorthodox layer creation so we can have baselayer above interaction layer.
+	// This comes from http://bl.ocks.org/rsudekum/5431771.
+	var topPane = map._createPane('leaflet-top-pane', map.getPanes().mapPane);
+	var topLayer = L.tileLayer('http://{s}.tiles.mapbox.com/v3/gabriel-florit.207de5da/{z}/{x}/{y}.png', {
+		minZoom: 7,
+		maxZoom: 13
+	}).addTo(map);
+	topPane.appendChild(topLayer.getContainer());
+	topLayer.setZIndex(6);
+
+	// Construct a new date with no minutes or seconds.
+	var date = new Date(_.chain(dates)
+		.pluck('date')
+		.sortBy(function(date) {
+			return date;
+		})
+		.first()
+		.value());
+
+	var hourDate = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes());
+
+	// Populate the 'updated' element.
+	$('.updated-timestamp').html('Updated ' + [APDateTime.time(hourDate), APDateTime.date(hourDate)].join(', '));
+
+};
+
+$.ajax({
+	url: 'http://www.bostonglobe.com/partners/outagescraper/outages.json',
+	dataType: 'jsonp',
+	jsonpCallback: 'outages'
+});
