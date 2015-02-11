bf0b80f59e0dd9cb24b8ac394387a57f7ce6e929
enatario <elaina.natario@gmail.com>
2015-02-11 14:25:54 -0500

    styling utility outage numbers

--- graphics/map/js/main.js
+++ graphics/map/js/main.js
@@ -238,12 +238,12 @@ window.outages = function(data) {
 			var displayDate = shortenedDateTime(date);
 			var label = v.outages === 1 ? 'outage' : 'outages';
 
-			return '<li>' + v.company + ': ' + numberWithCommas(v.outages) + ' ' + label + ' as of ' + displayDate + '</li>';
+			return '<li><span class="util-name">' + v.company + '</span><span class="util-outageinfo">' + numberWithCommas(v.outages) + ' ' + label + ' as of ' + displayDate + '</span></li>';
 		})
 		.value().join('');
 
 	// Populate the 'totals' element.
-	$('.subhed .totals span').html(numberWithCommas(statewideTotals));
+	$('.subhed .totals .totals-outage-no').html(numberWithCommas(statewideTotals));
 	$('.subhed ul').html(companiesHtml);
 
 	var popup;
